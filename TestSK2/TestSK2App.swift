48d5d6dbd6a22ae32e5eb9d480ec34b6af6cfd00
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-01-03 11:54:23 -0600

    Initial Commit

---
+++ TestSK2/TestSK2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestSK2App.swift
+//  TestSK2
+//
+//  Created by Andrew Fairchild on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TestSK2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
