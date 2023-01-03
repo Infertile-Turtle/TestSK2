7237c3516132856aff6d45b4dd8775648271162d
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-01-03 12:16:10 -0600

    Create test for StoreKit 2 App subscription 1 time purchase and recurring monthly/yearly subscription. Initial commit.

--- TestSK2/ContentView.swift
+++ TestSK2/ContentView.swift
@@ -6,21 +6,38 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    let productIds = ["pro_monthly", "pro_yearly", "pro_lifetime"]
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    @State
+    private var products: [Product] = []
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Products")
+            ForEach(self.products) { product in
+                Button {
+                    // Don't do anything yet
+                } label: {
+                    Text("\(product.displayPrice) - \(product.displayName)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.blue)
+                        .clipShape(Capsule())
+                }
+            }
+        }.task {
+            do {
+                try await self.loadProducts()
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private func loadProducts() async throws {
+        self.products = try await Product.products(for: productIds)
     }
 }
